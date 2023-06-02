37c5626fb9fd216b4de194fb9364e30bc93517e9
Edward·D·Lin <zhilinchn@gmail.com>
2023-06-03 03:49:10 +0800

    launcher icon and foreground service

--- app/src/main/java/com/mama/sensorDemo/MainActivity.java
+++ app/src/main/java/com/mama/sensorDemo/MainActivity.java
@@ -1,12 +1,23 @@
 package com.mama.sensorDemo;
 
 import android.app.Activity;
+import android.app.AppOpsManager;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
+
+import androidx.core.content.ContextCompat;
 
 import com.mama.R;
 import com.mama.screen.ScreenTool;
 
+import com.mama.service.MyForegroundService;
 import com.mama.service.SensorService;
 
 
@@ -15,7 +26,42 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent serviceIntent = new Intent(this, MyForegroundService.class);
+        ContextCompat.startForegroundService(this, serviceIntent);
+
+
+        //检查是否开通通知权限
+        if (!isForegroundServicePermissionGranted()) {
+            openForegroundServicePermissionSettings();
+        }
     }
+
+
+    private boolean isForegroundServicePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            return getSystemService(NotificationManager.class).areNotificationsEnabled();
+        } else {
+            String packageName = getPackageName();
+            AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+            ApplicationInfo appInfo = getApplicationInfo();
+            String op = AppOpsManager.OPSTR_GET_USAGE_STATS;
+            int mode = appOps.checkOpNoThrow(op, appInfo.uid, packageName);
+            return mode == AppOpsManager.MODE_ALLOWED;
+        }
+    }
+
+    private void openForegroundServicePermissionSettings() {
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                    .putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+        } else {
+            intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    .setData(Uri.fromParts("package", getPackageName(), null));
+        }
+        startActivity(intent);
+    }
+
 
     // 启动服务
     public void startService(View v) {
---
+++ app/src/main/java/com/mama/service/MyForegroundService.java
@@ -0,0 +1,122 @@
+package com.mama.service;
+
+import android.app.AppOpsManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.mama.R;
+import com.mama.screen.ScreenTool;
+import com.mama.sensor.SensorTool;
+
+public class MyForegroundService extends Service {
+
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "MyForegroundServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // 在这里执行后台任务
+        doMyJob();
+        // 创建前台服务通知
+        createNotificationChannel();
+        Notification notification = buildNotification();
+
+        // 将服务设置为前台服务
+        startForeground(NOTIFICATION_ID, notification);
+
+        // 如果服务被杀死，系统将尝试重新启动服务
+        return START_STICKY;
+    }
+
+
+
+    private void doMyJob() {
+        SensorTool.SetShakeListener(this, new SensorTool.ShakeListener() {
+            @Override
+            public void onShake() {
+                Toast.makeText(getApplicationContext(), "通过摇一摇，点亮屏幕", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean hasRecentAlarm() {
+        // 从SharedPreferences中获取上次设置闹钟的时间戳
+        SharedPreferences sharedPreferences = getSharedPreferences("AlarmPrefs", Context.MODE_PRIVATE);
+        long lastAlarmTime = sharedPreferences.getLong("LastAlarmTime", 0);
+
+        // 获取当前时间
+        long currentTime = System.currentTimeMillis();
+
+        // 判断上次设置闹钟的时间是否在最近的10分钟内
+        return (currentTime - lastAlarmTime) <= (10 * 60 * 1000); // 10分钟的毫秒数
+    }
+
+    private void setAlarmIfNeeded() {
+        if (!hasRecentAlarm()) {
+            // 未设置过最近的闹钟，设置一个2小时后的闹钟
+            long twoHoursLater = System.currentTimeMillis() + (2 * 60 * 60 * 1000); // 2小时的毫秒数
+
+            // TODO: 在这里设置闹钟，例如使用AlarmManager或其他闹钟相关的机制
+            // 这里只是一个示例，具体的设置闹钟逻辑需要根据你的应用和需求进行实现
+
+            // 更新上次设置闹钟的时间戳
+            SharedPreferences sharedPreferences = getSharedPreferences("AlarmPrefs", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putLong("LastAlarmTime", twoHoursLater);
+            editor.apply();
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 在服务销毁时执行清理操作
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Foreground Service Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("血糖闹钟")
+                .setContentText("为了更准时提醒请不要关闭")
+                .setSmallIcon(R.drawable.ic_clock);
+        return builder.build();
+    }
+}
